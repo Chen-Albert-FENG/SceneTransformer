50193abfb949a9ca4ba45cc8e561fedcab97b537
daeheepark <bag2824@gmail.com>
2021-08-09 20:59:32 +0900

    fix encoder

--- model/encoder.py
+++ model/encoder.py
@@ -48,31 +48,30 @@ class Encoder(nn.Module):
         self.layer_P = SelfAttLayer(self.time_steps,self.feature_dim,self.head_num,self.k,across_time=True)
         self.layer_Q = SelfAttLayer(self.time_steps,self.feature_dim,self.head_num,self.k,across_time=False)
         
-        self.layer_A_I = nn.Sequential(OrderedDict([('layer_A', self.layer_A),
-                                                    ('layer_D', self.layer_B),
-                                                    ('layer_E', self.layer_C),
-                                                    ('layer_F', self.layer_F),
-                                                    ('layer_G', self.layer_G),
-                                                    ('layer_H', self.layer_H),
-                                                    ('layer_I', self.layer_I),
-                                                    ]))
 
     def forward(self, state_feat, agent_batch_mask, road_feat, traffic_light_feat):
+        A_ = self.layer_A(state_feat)
         B_ = self.layer_B(traffic_light_feat)
         C_ = self.layer_C(road_feat)
-        I_ = self.layer_A_I(state_feat, batch_mask=agent_batch_mask)
+        
+        D_,_,_,_ = self.layer_D(A_,agent_batch_mask)
+        E_,_,_,_ = self.layer_E(D_,agent_batch_mask)
+        F_,_,_,_ = self.layer_F(E_,agent_batch_mask)
+        G_,_,_,_ = self.layer_G(F_,agent_batch_mask)
+        H_,_,_,_ = self.layer_H(G_,agent_batch_mask)
+        I_,_,_,_ = self.layer_I(H_,agent_batch_mask)
 
-        J_ = self.layer_J(I_,C_)
-        K_ = self.layer_K(J_,B_)
+        J_,_,_,_ = self.layer_J(I_,C_)
+        K_,_,_,_ = self.layer_K(J_,B_)
 
-        L_ = self.layer_L(K_)
-        M_ = self.layer_M(L_)
+        L_,_,_,_ = self.layer_L(K_,agent_batch_mask)
+        M_,_,_,_ = self.layer_M(L_,agent_batch_mask)
 
-        N_ = self.layer_N(M_,C_)
-        O_ = self.layer_O(N_,B_)
+        N_,_,_,_ = self.layer_N(M_,C_)
+        O_,_,_,_ = self.layer_O(N_,B_)
 
-        P_ = self.layer_P(O_)
-        Q_ = self.layer_Q(P_)
+        P_,_,_,_ = self.layer_P(O_,agent_batch_mask)
+        Q_,_,_,_ = self.layer_Q(P_,agent_batch_mask)
 
         return Q_
 
@@ -95,7 +94,7 @@ class SelfAttLayer(nn.Module):
 
         self.across_time = across_time
 
-    def forward(self, x, batch_mask, padding_mask, hidden_mask):
+    def forward(self, x, batch_mask, padding_mask=None, hidden_mask=None):
         K = self.layer_K_(x)
         V = self.layer_V_(x)
         Q0 = self.layer_Q0_(x)
@@ -103,14 +102,14 @@ class SelfAttLayer(nn.Module):
 
         if self.across_time:
             Q, K, V = Q.permute(0,2,1,3), K.permute(0,2,1,3), V.permute(0,2,1,3)    # Q,K,V -> [A,H,T,d]
-            energy = torch.matmul(Q,K.T) / self.scale                               # [A,H,T,T]
+            energy = torch.matmul(Q,K.permute(0,1,3,2)) / self.scale                               # [A,H,T,T]
             attention = torch.softmax(energy, dim=-1)                               # [A,H,T,T]
             Y1_ = torch.matmul(attention, V)                                        # [A,H,T,d]
             Y1_ = Y1_.permute(0,2,1,3).contiguous()                                 # [A,T,H,d]
 
         else:
             Q, K, V = Q.permute(1,2,0,3), K.permute(1,2,0,3), V.permute(1,2,0,3)    # Q,K,V -> [T,H,A,d]
-            energy = torch.matmul(Q,K.T) / self.scale                               # [T,H,A,A]
+            energy = torch.matmul(Q,K.permute(0,1,3,2)) / self.scale                               # [T,H,A,A]
             attention = torch.softmax(energy, dim=-1)                               # [T,H,A,A]
 
             if batch_mask is not None:                                              # batch_mask -> [A,A]
@@ -152,7 +151,7 @@ class CrossAttLayer(nn.Module):
         Q = self.layer_Q_(Q0)                                                       # [A,T,H,d]
 
         Q, K, V = Q.permute(1,2,0,3), K.permute(1,2,0,3), V.permute(1,2,0,3)    # Q -> [T,H,A,d] / K,V -> [T,H,G,d]
-        energy = torch.matmul(Q,K.T) / self.scale                               # [T,H,A,G]
+        energy = torch.matmul(Q,K.permute(0,1,3,2)) / self.scale                               # [T,H,A,G]
         attention = torch.softmax(energy, dim=-1)                               # [T,H,A,G]
 
         Y1_ = torch.matmul(attention, V)                                        # [T,H,A,d]
--- tmp.py
+++ tmp.py
@@ -5,7 +5,7 @@ from model.encoder import Encoder
 from datautil.waymo_dataset import WaymoDataset, waymo_collate_fn
 
 dataset = WaymoDataset('data')
-dataloader = DataLoader(dataset, batch_size=4, collate_fn=lambda x: waymo_collate_fn(x))
+dataloader = DataLoader(dataset, batch_size=1, collate_fn=lambda x: waymo_collate_fn(x))
 
 data0 = next(iter(dataloader))
 
@@ -20,7 +20,7 @@ state_feat, agent_batch_mask, road_feat, traffic_light_feat = data0
 #                                                                             traffic_light_feat.to(device)
 
 encoder = Encoder(in_feat_dim=state_feat.shape[-1], in_dynamic_rg_dim=traffic_light_feat.shape[-1], in_static_rg_dim=road_feat.shape[-1])
-encoder = encoder.to(device)
+# encoder = encoder.to(device)
 
 encodings = encoder(state_feat, agent_batch_mask, road_feat, traffic_light_feat)
 
