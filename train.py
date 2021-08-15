cf630a8c683a1b017a8258e5f59f41b96bbf5504
developer-account <irteam@cad103.labs>
2021-08-15 23:33:56 +0900

    a

--- train.py
+++ train.py
@@ -9,6 +9,8 @@ import pytorch_lightning as pl
 from datautil.waymo_dataset import WaymoDataset, waymo_collate_fn
 from model.pl_module import SceneTransformer
 
+
+
 @hydra.main(config_path='./conf', config_name='config.yaml')
 def main(cfg):
 
@@ -22,9 +24,10 @@ def main(cfg):
         print('Cached:   ', round(torch.cuda.memory_cached(GPU_NUM)/1024**3,1), 'GB')
 
     pl.seed_everything(cfg.seed)
-
-    dataset_train = WaymoDataset(cfg.train.tfrecords, cfg.train.idxs)
-    dataset_valid = WaymoDataset(cfg.valid.tfrecords, cfg.valid.idxs)
+    pwd = hydra.utils.get_original_cwd()
+    print('Current : ', pwd)
+    dataset_train = WaymoDataset(pwd+cfg.dataset.train.tfrecords, pwd+cfg.dataset.train.idxs)
+    dataset_valid = WaymoDataset(pwd+cfg.dataset.valid.tfrecords, pwd+cfg.dataset.valid.idxs)
     dloader_train = DataLoader(dataset_train, batch_size=cfg.dataset.batchsize, collate_fn=waymo_collate_fn)
     dloader_valid = DataLoader(dataset_valid, batch_size=cfg.dataset.batchsize, collate_fn=waymo_collate_fn)
 
