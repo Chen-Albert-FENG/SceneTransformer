026230f41cf8603bcd0fd7d8551ae4b121e57385
developer-account <irteam@cad103.labs>
2021-08-16 23:36:39 +0900

    a

--- datautil/create_idx.py
+++ datautil/create_idx.py
@@ -3,9 +3,9 @@ import os.path
 import glob
 from tqdm import tqdm
 
-tfrecord_path = './data/tf_example/training'
-idx_path = './data/idxs_training_bs_4'
-batch_size = 4
+tfrecord_path = './data/tf_example/validation'
+idx_path = './data/idxs_validation_bs_1'
+batch_size = 1
 
 for tfrecord in tqdm(glob.glob(tfrecord_path+'/*')):
     idxname = idx_path + '/' + tfrecord.split('/')[-1]
