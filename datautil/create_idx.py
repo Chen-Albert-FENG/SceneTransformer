a53743ffa2f749e65e070ba4689cd5209659e4fb
developer-account <irteam@cad103.labs>
2021-08-18 11:58:09 +0900

    a

--- datautil/create_idx.py
+++ datautil/create_idx.py
@@ -3,12 +3,15 @@ import os.path
 import glob
 from tqdm import tqdm
 
-tfrecord_path = './data/tf_example/validation'
-idx_path = './data/idxs_validation_bs_2'
-batch_size = 2
+#tfrecord_path = './data/tf_example/validation'
+#idx_path = './data/idxs_validation_bs_2'
+#batch_size = 2
+
+tfrecord_path = './data/tfrecords'
+idx_path = './data/idxs'
+batch_size=2
 
 for tfrecord in tqdm(glob.glob(tfrecord_path+'/*')):
     idxname = idx_path + '/' + tfrecord.split('/')[-1]
-    if not os.path.isfile(idxname):
-        
+    if not os.path.isfile(idxname):  
         call(["tfrecord2idx", tfrecord, idxname])
